077c0337bd92c9ce78326e8a66af01293f3e54ff
Unnati Ghodki <37828502+unnatighodki@users.noreply.github.com>
2023-12-11 23:11:46 -0500

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,5 +1,15 @@
-print("Initializing the Python Package for Natural Language Processing")
 
-import PorterStemmer
-import Preprocess
-import NaiveBayes
+
+try:
+    print("Initializing the Python Package for Natural Language Processing")
+    import PorterStemmer
+    import Preprocess
+    import NaiveBayes
+
+    import_error = None
+
+except Exception as ex:
+    import_error = ex
+
+def test_import_nlp():
+    assert import_error is None
